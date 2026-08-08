226b269c94b107d1b9f8f4674cf6c31281a21f82
NdaAgah <35528163+NdaAgah@users.noreply.github.com>
2026-08-08 11:19:37 +0700

    Update app.js
    URL v1

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
   initNeonGrid('neonCanvas');
 
   // 2. Tempelkan URL Web App GAS Anda di sini
-  const GAS_URL = "https://script.google.com/macros/s/AKfycbwsYkqOegWanTuH4YqWYW3gK7j6FQnI_EcsFcBmSHaqBkm_-dGaZ9YYgN3iQNww6yRr-w/exec";
+  const GAS_URL = "https://script.google.com/macros/s/AKfycbwmESmyxp7xr_QK3p0ocOUm0xrVpSxWqS6NAMjdgGb0Yt7c36t1qMxZl3x42TGxc-Bi5Q/exec";
 
   const btnFetch = document.getElementById('btnFetch');
   const dataOutput = document.getElementById('dataOutput');
