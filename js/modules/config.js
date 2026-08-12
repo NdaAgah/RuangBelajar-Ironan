3109240c7577c8ca840f71cb29e58bf99e49fb95
NdaAgah <35528163+NdaAgah@users.noreply.github.com>
2026-08-12 22:16:23 +0700

    perbaikan minor 1

--- js/modules/config.js
+++ js/modules/config.js
@@ -1,3 +1,3 @@
 export const CONFIG = {
-    GAS_URL = "https://script.google.com/macros/s/AKfycby0y5re-ti_61pFeGcl6C1PgFhR1fcuYIOug50ejSiwDuInp793xDfzg6mqcfZKpLLzJQ/exec"
+    GAS_URL: "https://script.google.com/macros/s/AKfycby0y5re-ti_61pFeGcl6C1PgFhR1fcuYIOug50ejSiwDuInp793xDfzg6mqcfZKpLLzJQ/exec"
 };
